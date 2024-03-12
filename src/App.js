6380f16d951dc3b1ef0da7c24bbd7178a081f24a
Mahdiar <mahdyarvaez@gmail.com>
2024-03-12 06:44:46 -0700

    style and icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import "./App.css";
+import { IoTrashBinOutline } from "react-icons/io5";
+import { FaCheck } from "react-icons/fa6";
+
 export default function App() {
+  const [completeTab, setCompleteTab] = useState(true);
+  const [title, setTitle] = useState();
+  const [des, setDes] = useState();
+  const list = [];
+
   return (
     <div>
       <h3 className="title">To Do List </h3>
@@ -8,25 +16,71 @@ export default function App() {
         <div className="inputs">
           <div>
             <label>title</label>
-            <input type="text" placeholder="Add Your Title" />
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+              placeholder="Add Your Title"
+            />
           </div>
           <div>
             <label>Description</label>
-            <input type="text" placeholder="Add Your Des" />
+            <input
+              value={des}
+              onChange={(e) => setDes(e.target.value)}
+              type="text"
+              placeholder="Add Your Des"
+            />
           </div>
           <div className="btn">
-            <button>Add </button>
+            <button
+              onClick={() => {
+                if (title && des) {
+                  list.unshift({ ...list, title: title, des: des });
+                  setTitle("");
+                  setDes("");
+                  console.log(list);
+                }
+              }}
+            >
+              Add{" "}
+            </button>
           </div>
         </div>
 
         <div className="btn-area">
-          <button>To Do</button>
-          <button>Completed</button>
+          <button
+            onClick={() => setCompleteTab(false)}
+            className={!completeTab ? "active-tab" : ""}
+          >
+            To Do
+          </button>
+          <button
+            onClick={() => setCompleteTab(true)}
+            className={completeTab ? "active-tab" : ""}
+          >
+            Completed
+          </button>
         </div>
-      
-      </div>
-      <div className="tasks">
-        
+        <div className="tasks">
+          <div className="task-content">
+            <h3 >title</h3>
+            <p>Description</p>
+          </div>
+          <div className="task-icons">
+              <FaCheck className="check-icon" style={{
+                cursor:'pointer'
+                ,fontSize:'40px',
+                '&:hover':{
+                  color:'red !important'
+                }
+              }}/>
+              <IoTrashBinOutline className="bin-icon" style={{
+                  cursor:'pointer'
+                  ,fontSize:'40px'
+              }}/>
+          </div>
+        </div>
       </div>
     </div>
   );

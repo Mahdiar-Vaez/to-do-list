958f5c34459b81c9e4011f7eea62a057ccd8ee99
Mahdiar <mahdyarvaez@gmail.com>
2024-03-12 04:33:39 -0700

    create element and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3 className="title">To Do List </h3>
+      <div className="wrapper">
+        <div className="inputs">
+          <div>
+            <label>title</label>
+            <input type="text" placeholder="Add Your Title" />
+          </div>
+          <div>
+            <label>Description</label>
+            <input type="text" placeholder="Add Your Des" />
+          </div>
+          <div className="btn">
+            <button>Add </button>
+          </div>
+        </div>
+
+        <div className="btn-area">
+          <button>To Do</button>
+          <button>Completed</button>
+        </div>
+      
+      </div>
+      <div className="tasks">
+        
+      </div>
     </div>
   );
 }
-
-export default App;
